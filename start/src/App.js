47c2bb1bffe2a25987c602da50c55bd7f50089e6
이준 <dlwnssla@jfomalhaut.com>
2020-03-09 18:10:29 +0900

    200309_2

--- start/src/App.js
+++ start/src/App.js
@@ -1,67 +1,68 @@
 import React, { useState } from 'react'
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import Navigation from './components/Navigations';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigations';
+import List from './router/List';
+import Home from './router/Home';
 
-// const App = () => {
-// 	const [count, setCount] = useState(0);
-// 	const increasement = () => {
-// 		setCount(count + 1);
-// 	};
+const App = () => {
+	const [count, setCount] = useState(0);
+	const increasement = () => {
+		setCount(count + 1);
+	};
 
-// 	return(
-// 		<div>
-// 			<h1>hi</h1>
-// 			<h2>{count}</h2>
-// 			<button onClick={increasement}>increasement</button>
-// 		</div>
-// 	);
+	// return(
+	// 	<div>
+	// 		<h1>hi</h1>
+	// 		<h2>{count}</h2>
+	// 		<button onClick={increasement}>increasement</button>
+	// 	</div>
+	// );
 
-// 	return (
-// 		<BrowserRouter>
-// 			<Navigation />
-// 			<Route path="/" exact={true} component={Home} />
-// 			<Route path="/about" component={About} />
-// 			<Route path="/movie/:id" component={Detail} />
-// 		</BrowserRouter>
-// 	)
+	return (
+		<BrowserRouter>
+			<Navigation />
+			<Route path="/" exact={true} component={Home} />
+			<Route path="/list" component={List} />
+		</BrowserRouter>
+	)
+};
+
+export default App;
+
+// export default class App extends React.Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			count: 10
+// 		};
+// 		this.increasement = this.increasement.bind(this);
+// 		this.decreasement = this.decreasement.bind(this);
+// 	}
+
+// 	componentDidMount() {
+// 		console.log('componentDidMount');
+// 	}
+
+// 	componentDidUpdate(){
+// 		console.log('componentDidUpdate');
+// 	}
+
+// 	increasement () {
+// 		this.setState({ count: this.state.count + 1});
+// 	}
+
+// 	decreasement () {
+// 		this.setState({ count: this.state.count - 1});
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="container">
+// 				<h1>hi! I'm App Component</h1>
+// 				<h2>{this.state.count}</h2>
+// 				<button onClick={this.increasement}>increase</button>
+// 				<button onClick={this.decreasement}>decrease</button>
+// 			</div>
+// 		);
+// 	}
 // };
-
-// export default App;
-
-export default class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			count: 10
-		};
-		this.increasement = this.increasement.bind(this);
-		this.decreasement = this.decreasement.bind(this);
-	}
-
-	componentDidMount() {
-		console.log('componentDidMount');
-	}
-
-	componentDidUpdate(){
-		console.log('componentDidUpdate');
-	}
-
-	increasement () {
-		this.setState({ count: this.state.count + 1});
-	}
-
-	decreasement () {
-		this.setState({ count: this.state.count - 1});
-	}
-
-	render() {
-		return (
-			<div className="container">
-				<h1>hi! I'm App Component</h1>
-				<h2>{this.state.count}</h2>
-				<button onClick={this.increasement}>increase</button>
-				<button onClick={this.decreasement}>decrease</button>
-			</div>
-		);
-	}
-}
