c87785dd31191d3ed78ea90c64f17fee18013748
이준 <dlwnssla@jfomalhaut.com>
2020-03-12 19:59:51 +0900

    dd

---
+++ corona/src/components/Store.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { IoMdStar } from "react-icons/io";
+import './Store.css';
+
+const Store = ({ test, item: { name, addr, type, stock_at, remain_stat, lat, lng, created_at } }) => {
+	const [pkg, setPkg] = useState([]);
+
+	const getStock = label => {
+		switch (label) {
+			case 'plenty': return '100개 이상';
+			case 'some': return '30개 ~ 100개';
+			case 'few': return '2개 ~ 30개';
+			case 'empty': return '재고없음';
+		}
+	};
+
+	const getType = type => {
+		switch (type) {
+			case '01': return '약국';
+			case '02': return '우체국';
+			case '03': return '농협';
+		}
+	};
+
+	const getPackage = (value) => {
+		switch (value) {
+			case 'plenty': setPkg([true, true, true, true, true]); break;
+			case 'some': setPkg([true, true, true, false, false]); break;
+			case 'few': setPkg([true, true, false, false, false]); break;
+			case 'empty': setPkg([false, false, false, false, false]); break;
+		}
+	};
+
+	const activePackage = (flag) => {
+		if (flag) return 'active';
+		else { return '' }
+	}
+
+	useEffect(() => {
+		getPackage(remain_stat);
+	}, []);
+
+	return (
+		<div className="store">
+			<div className="stocks">
+				<div className={`${remain_stat} stockDesc`}>{getStock(remain_stat)}</div>
+				<ul className="stock">
+					{pkg.map(item => (
+						<li className={`pkg ${activePackage(item)}`}><IoMdStar/></li>
+					))}
+				</ul>
+			</div>
+			<div className="storeName">
+				<div className="name">{name}</div>
+				<div className="type">{getType(type)}</div>
+			</div>
+			<div className="address">{addr}</div>
+			{/* <div>최종입고 {moment(stock_at).format('MM월DD일 HH시mm분')}</div> */}
+		</div>
+	);
+};
+
+export default Store;
--- corona/src/routes/Home.js
+++ corona/src/routes/Home.js
@@ -1,31 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+import Store from '../components/Store';
+import './Home.css';
+import { KakaoMap } from "react-kakao-maps";
+
+const APIKEY = '//dapi.kakao.com/v2/maps/sdk.js?appkey=d8373fa46b12a66aa806652efcf6aa50';
 
 const Home = () => {
 	const [location, setLocation] = useState([]);
 	const [stores, setStores] = useState([]);
 
 	const getGeolocation = () => {
-		if (navigator.geolocation) {
-			console.log(navigator.geolocation);
-			navigator.geolocation.getCurrentPosition((position) => {
-				setLocation({
-					lat: position.coords.latitude,
-					lng: position.coords.longitude
-				});
+		if ("geolocation" in navigator) {
+			navigator.geolocation.watchPosition((position) => {
+				const { coords: { latitude, longitude } } = position;
+				getDrugStore(latitude, longitude);
+				setLocation({ lat: latitude, lng: longitude });
 			});
 		}
 	};
 
-	const getDrugStore = () => {
-		Axios.get(`https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=${location.lat}&lng=${location.lng}&m=1000`).then(res => {
-			console.log(res);
+	const getDrugStore = (lat, lng) => {
+		Axios.get(`https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=${lat}&lng=${lng}&m=2000`).then(res => {
+			const { data: { count, stores } } = res;
+			const data = stores.filter(item => {
+				return item.remain_stat;
+			})
+			setStores(data);
 		});
-	}
-	
+	};
+
 	useEffect(() => {
-		// console.log(location);
-		// getDrugStore();
+		console.log(location);
 	}, [location]);
 
 	useEffect(() => {
@@ -33,7 +39,23 @@ const Home = () => {
 	}, []);
 
 	return (
-		<h1>Hello, React!</h1>
+		<div className="container">
+			<div className="stores">
+				{stores.length > 0 && stores.map((item, index) => (
+					<Store key={`Store${item.code}_${index}`} item={item} />
+				))}
+			</div>
+			<div className="map">
+				{/* <KakaoMap
+					apiUrl={APIKEY}
+					width="100%"
+					height="700px"
+					level={2}
+					lat={37.490826}
+					lng={127.03342}
+				></KakaoMap> */}
+			</div>
+		</div>
 	);
 };
 
